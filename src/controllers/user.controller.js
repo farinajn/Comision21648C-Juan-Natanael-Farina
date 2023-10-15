96af08859f90d630f71752c95273b418a1f51ae4
farinajn <farinajn@gmail.com>
2023-10-15 12:08:23 -0300

    Se crea el modelo de datos de los posts y el archivo de post.controllers.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,57 @@
 const User = require("../models/user.models");
 
-const ctlrUsers = {};
+// const controllerUsers = {};
+
+// controllerUsers.getAllUsers = async (req,res) => {
+//   // return await User.findAll();
+//   return { titleUser: "Usuarios", results: await User.findAll() }
+
+//   // res.render("user", { titleUser: "Usuarios", results: users });
+// };
+
+// controllerUsers.deleteAllUsers = async (req, res) => {
+// };
+
+// module.exports = controllerUsersInLine;
+
+// // routes
+// const userRouter = require("express").Router();
+
+// // http://localhost:3000/user
+// userRouter.get("/", async (req,res)=>{
+//   const data = await controllerUsers.getAllUsers(req,res);
+
+//   res.render("user", data);
+// });
+
+// // http://localhost:3000/user/1
+// userRouter.get("/:id", async (req,res)=>{
+//   const data = await controllerUsers.getAllUsers(req,res);
+
+//   res.render("user", data);
+// });
+
+// // en app.js
+// // http://localhost:3000
+// app.get('/', (req, res) => {
+//   res.render('index', { title: 'Home' });
+// });
+
+// app.use("/user", userRouter);
+
+// // http://localhost:3000/user
+
+// // controllers
+// // funciones que ejecutan algo y devuelven algo
+
+// // routes
+// // rutas que ejecutan un controlador y devuelven una vista
+
+// // views
+// // templates que se parsean con datos y devuelven html
+// // ESTO NO ES HTML<%= titleUser %> ejs --> HTML <div>Usuarios</div>
+// // {{ titleUser }} handlebars --> HTML <div>Usuarios</div>
+// // jsx react return <div>{titleUser}</div> --> HTML <div>Usuarios</div>
 
 //CRUD
 
@@ -18,9 +69,9 @@ module.exports = {
 
     if (!user) return null;
 
-    const isMatch = compareUser(user, password);
+    const isPasswordValid = compareUser(user, password);
 
-    if (!isMatch) return null;
+    if (!isPasswordValid) return null;
 
     return user;
   },
@@ -38,5 +89,9 @@ module.exports = {
     } catch (error) {
       return error.message;
     }
+  },
+  getById: async (id) => {
+    if (!id) return null;
+    return await User.findById(id);
   },
 };
--- src/models/post.models.js
+++ src/models/post.models.js
@@ -1,16 +1,20 @@
-// const { DataTypes, Model } = require("sequelize");
+const { DataTypes, Model } = require("sequelize");
 
-// class Post extends Model {}
+const { db } = require("../database/db");
 
-// module.exports = (db) => {
-//   return Post.init(
-//     {
-//       id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-//       title: { type: DataTypes.STRING, allowNull: false },
-//       content: { type: DataTypes.STRING, allowNull: false },
-//       link: { type: DataTypes.STRING, allowNull: false },
-//       image: { type: DataTypes.STRING, allowNull: false },
-//     },
-//     { sequelize: db, timestamps: true, modelName: "post" }
-//   );
-// };
+const Post = db.define(
+  "post",
+  {
+    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+    title: { type: DataTypes.STRING, allowNull: false },
+    content: { type: DataTypes.STRING(2000), allowNull: false },
+    date: { type: DataTypes.DATE, allowNull: false },
+    image: { type: DataTypes.STRING(2000), allowNull: false },
+  },
+  {
+    timestamps: true,
+    modelName: "post",
+  }
+);
+
+module.exports = Post;
